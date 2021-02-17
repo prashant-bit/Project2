4825f21dd66b4d224ca25c2412674d9a41154bd2
PSR <ju.prashantsingh@hmail.com>
2021-02-17 17:21:16 +0530

    Add project files.

---
+++ PROJECT_3/Controllers/StudentController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROJECT_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJECT_3.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _Db;
+        public StudentController(StudentContext Db)
+        {
+            _Db = Db;
+        }
+        public IActionResult StudentList()
+        {
+            var s = from a in _Db.tbl_Student
+                    join b in _Db.tbl_Departments
+                    on a.DepId equals b.Id
+                    into Dep
+                    from b in Dep.DefaultIfEmpty()
+
+                    select new Student
+                    {
+                        Id = a.Id,
+                        name = a.name,
+                        DepId = a.DepId,
+
+                        DepName = b == null ? "" : b.name
+                    };
+            var stdList = _Db.tbl_Student;
+            return View(s);
+        }
+
+        public IActionResult Create(Student obj)
+        {
+            loadDDL();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent (Student obj)
+        {
+            if(ModelState.IsValid)
+            {
+                if(obj.Id == 0)
+                {
+                    _Db.tbl_Student.Add(obj);
+                    await _Db.SaveChangesAsync();
+                   
+                }
+                else
+                {
+                    _Db.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    await _Db.SaveChangesAsync();
+                }
+
+                return RedirectToAction("StudentList");
+
+            }
+            return View();
+        }
+
+        private void loadDDL()
+        {
+            List<Departments> depList = new List<Departments>();
+            depList = _Db.tbl_Departments.ToList();
+
+            ViewBag.DepList = _Db.tbl_Departments;
+        }
+    }
+}
---
+++ PROJECT_3/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJECT_3.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int DepId { get; set; }
+
+        [NotMapped]
+        public string DepName { get; set; }
+
+    }
+}
